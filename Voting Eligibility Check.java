02740c2718314120c993df205f0a7fa291954cb8
Kannan <105408067+kannan202699@users.noreply.github.com>
2022-05-14 09:07:48 -0700

    Create Voting Eligibility Check.java
    Learing

---
+++ Voting Eligibility Check.java
@@ -0,0 +1,15 @@
+package Pseudo;
+import java.util.Scanner;
+public class Voting 
+{
+	public static void main(String[] args) 
+	{
+        int age;
+        @SuppressWarnings("resource")
+		Scanner scann = new Scanner(System.in);
+        System.out.println("Enter the age");
+        age=scann.nextInt();
+        System.out.println(age>=18 ? "Ready to vote" : "You are not eligible");
+	}
+
+}
