c1e572304c37b7901b4e1ee7c0238b0c7df47633
Kannan <105408067+kannan202699@users.noreply.github.com>
2022-05-14 09:51:34 -0700

    Create Bio.java
    FuN

---
+++ Bio.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Bio
+{
+	public static void main(String[] args) 
+	{
+    System.out.println("AKASH");
+    System.out.println("BROOO");
+    System.out.println("KAUSHIK");
+    System.out.println("SAM");
+    System.out.println("SINGH");
+	}
+
+}
