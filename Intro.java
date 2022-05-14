b0093f1fa36e48c2948d01e75ba522209202de50
Kannan <105408067+kannan202699@users.noreply.github.com>
2022-05-14 09:37:39 -0700

    Create Intro.java
    Fun

---
+++ Intro.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class example1 {
+	public static void main(String args[])
+	{	  
+		 System.out.println("Hello World");  
+		 System.out.println("Keep Calm.!");
+		 System.out.println("I'm Here...");
+	}
+}
